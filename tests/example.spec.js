b3de456b6e70f5231e5214dc2b1ad843d738aaf0
GONRONDEROS <gonzalorond@gmail.com>
2026-02-11 15:12:00 -0300

    adicion del caso para cuando se necesita mayor especificidad en productos a buscar

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,21 +6,34 @@ const {ProductsPage} = require('../pageobjects/productsPage');
 const dataSet = JSON.parse(JSON.stringify(require('../utils/mainManuCategoriesOptions.json')));
 
 for (const data of dataSet){
-test(`Navegar a "${data.category}", "${data.particularCategory}"`, async ({ page }) => {
+test(`Navegar a "${data.category}", "${data.particularCategory}", "${data.moreParticularCategory}"`, async ({ page }) => {
   await page.goto('https://www.cotodigital.com.ar/sitios/cdigi/nuevositio');
 
   const mainMenuBar = new MainMenuBar(page);
   const productPage = new ProductsPage(page);
+
   await mainMenuBar.hoverCategoryMenu(data.menuCategory);
   await mainMenuBar.selectAndNavigateToParticularCategory(data.category, data.particularCategory);
   expect(page.url()).toContain(data.particularCategory.toLowerCase());
+
+  if(data.moreParticularCategory){
+    await productPage.clickFiltroDeProductos(data.moreParticularCategory);
+    for(const option of data.specificCatefory){
+    
+    const url = await productPage.clickFiltroDeProductos(option);
+    const apiProducts = new APIProducts(url.split('?')[0]);
+    const products = await apiProducts.listProducts();
+    console.log(products);
+    await page.goBack();
+    }
+  } else {
   for(const option of data.specificCatefory){
-    //await productPage.clickFiltroDeProductos(option);
+    
     const url = await productPage.clickFiltroDeProductos(option);
     const apiProducts = new APIProducts(url.split('?')[0]);
     const products = await apiProducts.listProducts();
     console.log(products);
     await page.goBack();
   }
-})
+}})
 };
